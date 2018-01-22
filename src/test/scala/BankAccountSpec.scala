e865985a82bedde02d5747d570ff823f4293fb9e
Jonathan Duberville <jduberville@octo.com>
2018-01-22 23:07:07 +0100

    Initialisation

---
+++ src/test/scala/BankAccountSpec.scala
@@ -0,0 +1,10 @@
+import org.scalatest._
+
+class BankAccount {
+}
+
+class BankAccountSpec extends FlatSpec with Matchers {
+  "BankAccount balance" should "be 0 at start" in {
+    true should equal(false)
+  }
+}
